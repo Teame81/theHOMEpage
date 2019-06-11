263f3be7361e3d7421bb3c0d3327be96b49ccfee
Timmie Nilsson <timmie.nilsson@gmail.com>
2019-06-11 14:00:10 +0200

    Comments2

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@
 from flask_wtf import  FlaskForm
 from wtforms import SubmitField, RadioField, StringField
 
+#Creating a form
 class TimmieForm(FlaskForm):
     timmiehome = RadioField('Change?', choices=[('True','Hemma'),('False','Borta')])
     submit = SubmitField('Skicka')
